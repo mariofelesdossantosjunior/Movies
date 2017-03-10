748b4b4148a6a801e40b31310d9c7fa20fd10aca
mario <mario_feles@live.com>
2017-03-10 17:59:29 -0300

    databinding

---
+++ app/src/main/java/com/mario/movies/view/IMainActivity.java
@@ -0,0 +1,12 @@
+package com.mario.movies.view;
+
+/**
+ * @author Mario Feles dos Santos Junior
+ * @email <mario_feles@live.com/>
+ * @project Movies
+ * @since 10/03/17 - 17:43
+ */
+
+public interface IMainActivity {
+
+}
